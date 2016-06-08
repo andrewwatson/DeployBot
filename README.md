1eb4d9c4fe7c6d7e4c55e1061a8f63d8a25dbe87
Andrew Watson <andy@ionic.com>
2016-06-07 23:26:17 -0400

    initial verification steps

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DeployBot
 This is a bot that gets incoming webhooks (from slack first) and can deploy a Go app to a Google App Engine Flexible VM
+
+## Configuration
+You'll have to provide DeployBot with the correct token in order to validate the incoming hooks from Slack
--- main.go
+++ main.go
@@ -1,9 +1,92 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+var (
+	router                              *mux.Router
+	slackToken, slackChannel, slackTeam string
+)
+
+func init() {
+	router = mux.NewRouter()
+
+	slackToken = os.Getenv("SLACK_TOKEN")
+	if slackToken == "" {
+		log.Fatal("YOU HAVE TO SET SLACK_TOKEN")
+	}
+
+	slackChannel = os.Getenv("SLACK_CHANNEL")
+	if slackChannel == "" {
+		log.Fatal("YOU HAVE TO SET SLACK_CHANNEL")
+	}
+
+	slackTeam = os.Getenv("SLACK_TEAM")
+	if slackTeam == "" {
+		log.Fatal("YOU HAVE TO SET SLACK_TEAM")
+	}
+
+}
 func main() {
-	fmt.Println("hey")
+
+	// GET HEAD POST PUT OPTIONS DELETE TRACE CONNECT
+	router.Methods("HEAD", "OPTIONS", "TRACE", "CONNECT", "PUT", "DELETE").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Nope!", http.StatusMethodNotAllowed)
+	})
+
+	router.Methods("POST").Path("/deploy").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		/*
+		   token=AzwMDTFpcgdD3YtPMYHpsRQu
+		   team_id=T0001
+		   team_domain=example
+		   channel_id=C2147483705
+		   channel_name=test
+		   user_id=U2147483697
+		   user_name=Steve
+		   command=/weather
+		   text=94070
+		   response_url=https://hooks.slack.com/commands/1234/5678
+		*/
+		incomingToken := r.FormValue("token")
+		incomingChannel := r.FormValue("channel_name")
+		incomingTeam := r.FormValue("team_domain")
+
+		if incomingToken != slackToken {
+			http.Error(w, "BAD TOKEN", http.StatusUnauthorized)
+			return
+		}
+
+		if slackChannel != incomingChannel {
+			http.Error(w, "BAD CHANNEL", http.StatusForbidden)
+			return
+		}
+
+		if slackTeam != incomingTeam {
+			http.Error(w, "BAD TEAM", http.StatusForbidden)
+			return
+		}
+
+		w.Write([]byte("thanks\n"))
+
+		return
+
+	})
+
+	http.Handle("/", router)
+	httpServer := http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(httpServer.ListenAndServe())
 }
